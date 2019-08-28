375bd4f0f8b4ee302a25c2712b2c4e27bcafb1aa
wilson000 <53593353+wilson000@users.noreply.github.com>
2019-08-27 21:47:19 -0500

    Add files via upload

---
+++ programa1.3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+#include<cstdlib>
+using namespace std;
+main() 
+{
+cout<<"buenas noches";
+cout<<endl; 
+cout<<"es tarde \n"; cout<<"seran las 10"; 
+cout<<endl;
+
+getch();
+system("pause");
+}
